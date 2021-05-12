af661dc3fba13dd551990b457b584927a3bd4ca8
elicheva katz <elisheva.g@simplyct.co.il>
2021-05-12 09:41:54 +0300

    add share button to product page

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -47,8 +47,16 @@ if ( $product->is_in_stock() ) : ?>
 		?>
 
 		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-		
-		<a class="product-share-popup">Share</a>
+		<!-- <a class="product-share-popup">Share</a> -->
+		 <div class="product-share-wrapper">
+		 	<?php 
+			global $wp;
+			$current_url = home_url( $_SERVER['REQUEST_URI'] );
+			echo do_shortcode('[addtoany url="' . $current_url . '" title="' . esc_html( $product->single_add_to_cart_text()) . '"]') ;
+			?>
+		 </div> 
+
+	
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
